aa262a8951ccb1cafb0774d76a6fe4762faf9199
Michael Justin Brandt <mbrandt@cse.unl.edu>
2018-08-23 14:33:08 -0500

    Added Source Files

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Michael Brandt
+ * Date: 2018/23/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Michael Brandt, Computer Engineering!\n");
+
+  return 0;
+}
